f52559dc8ada48f4ca97663d224e22fc6ed8c004
Bharat Panwar <55885130+bsingh23@users.noreply.github.com>
2021-01-01 20:37:58 +0530

    Update script fortinet_firewall_address.py
    Updated script with following improvements
    1. Added error handling to check for correct filename
    2. Added error handling to check for correct IP address or IP-Network's
    3. Remove whitespaces from IP-Address file

--- Fortinet firewall object script/fortinet_firewall_address.py
+++ Fortinet firewall object script/fortinet_firewall_address.py
@@ -1,26 +1,31 @@
 #!/usr/bin/python3
 
 import datetime
+import ipaddress
 
 time_now = datetime.datetime.now().isoformat(timespec="seconds")
 
-#Give the name  of file that contain IP-Address information
+#Give the name of file that contain IP-Address information
 filename = input("Enter the name of file to open: ")
 
 #Open File, Read content and create a List
-with open(filename) as f:  
-   ip_addrs = f.read() 
+#Error Handling - To check for provided FileName and valid IP address
+try:
+    with open(filename) as f:
+        ip_addr_list = f.read().splitlines()
 
-ip_addr_list = ip_addrs.split()
-
-#For loop that will create the configuration file
-with open (filename + "_"  + time_now + ".txt", "w") as f:
-    f.write("config firewall address\n")
-    for ip in ip_addr_list:
-        f.write(f"edit {filename}_{ip}") 
-        f.write("\n")
-        f.write(f"set subnet {ip}")
-        f.write("\n")
-        f.write("next")
-        f.write("\n")
-    f.write("end")
+        with open (filename + "_"  + time_now + ".txt", "w") as f:
+            f.write("config firewall address \n")
+            for ip in ip_addr_list:
+                ip = ip.strip()
+                try:
+                    #ipaddress module to check if the provided IP address or IP-NETWORK's are valid
+                    if bool(ipaddress.ip_network(ip)):
+                        f.write(f"edit {filename}_{ip} \n")
+                        f.write(f"set subnet {ip} \n")
+                        f.write("next \n")
+                except ValueError:
+                    print(f"{ip} does not appear to be an IPv4 or IPv6 network. Please check in File.")
+            f.write("end")
+except FileNotFoundError:
+    print("File not found or incorrect file name")
